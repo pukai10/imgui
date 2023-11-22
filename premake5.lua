90ef9ec2e71a5c807f7d058b6ccf6f69566feadc
aurogonpu <aurogonpu@tencent.com>
2023-11-22 19:32:06 +0800

    修改premake

--- premake5.lua
+++ premake5.lua
@@ -1,10 +1,10 @@
 project "ImGui"
 	kind "StaticLib"
 	language "C++"
-    staticruntime "off"
+    staticruntime "on"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-obj/" .. outputdir .. "/%{prj.name}")
 
 	files
 	{
@@ -16,7 +16,54 @@ project "ImGui"
 		"imgui_tables.cpp",
 		"imgui_widgets.cpp",
 		"imstb_rectpack.h",
-		"imstb_textedit.h",
+		"imstb_textedit.h",project "ImGui"
+		kind "StaticLib"
+		language "C++"
+		staticruntime "on"
+	
+		targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+		objdir ("bin-obj/" .. outputdir .. "/%{prj.name}")
+	
+		files
+		{
+			"imconfig.h",
+			"imgui.h",
+			"imgui.cpp",
+			"imgui_draw.cpp",
+			"imgui_internal.h",
+			"imgui_tables.cpp",
+			"imgui_widgets.cpp",
+			"imstb_rectpack.h",
+			"imstb_textedit.h",
+			"imstb_truetype.h",
+			"imgui_demo.cpp"
+		}
+	
+		filter "system:windows"
+			systemversion "latest"
+			cppdialect "C++17"
+	
+		filter "system:linux"
+			pic "On"
+			systemversion "latest"
+			cppdialect "C++17"
+	
+		filter "system:macosx"
+				systemversion "latest"
+				cppdialect "C++17"
+	
+		filter "configurations:Debug"
+			runtime "Debug"
+			symbols "on"
+	
+		filter "configurations:Release"
+			runtime "Release"
+			optimize "on"
+	
+		filter "configurations:Dist"
+			runtime "Release"
+			optimize "on"
+			symbols "off"
 		"imstb_truetype.h",
 		"imgui_demo.cpp"
 	}
